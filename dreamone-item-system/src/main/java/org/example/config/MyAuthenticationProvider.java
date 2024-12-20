5d8a3601c6327c535b0ec2befaccdbeb1a060b64
恬裕 <patiencehu@126.com>
2024-12-20 13:59:52 +0800

    fix: add application.properties and config

---
+++ dreamone-item-system/src/main/java/org/example/config/MyAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package org.example.config;
+
+import org.example.constant.CommonConstants;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        String role_admin = "ROLE_ADMIN";
+        // String role = "ROLE_GUEST";
+
+        if (CommonConstants.LOGIN_USER_MAP.containsKey(username) && CommonConstants.LOGIN_USER_MAP.get(username).equals(password)) {
+            List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
+            grantedAuths.add(new SimpleGrantedAuthority(role_admin));
+            return new UsernamePasswordAuthenticationToken(username, password, grantedAuths);
+        }
+        throw new BadCredentialsException(username);
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
