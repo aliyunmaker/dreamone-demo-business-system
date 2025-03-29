07c5fecbfabde09ee417e70afd1829264ad736f5
daxingplay <daxingplay@gmail.com>
2025-03-29 15:57:18 +0800

    fix: add customer view

--- dreamone-customer-system/src/main/java/org/example/controller/CustomerController.java
+++ dreamone-customer-system/src/main/java/org/example/controller/CustomerController.java
@@ -21,6 +21,7 @@ import org.example.task.ExceptionTask;
 import org.example.utils.RequestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -38,6 +39,11 @@ public class CustomerController extends BaseController {
     ExceptionTask exceptionTask;
 
     private Boolean simulateError = false;
+
+    @GetMapping("/customer")
+    public String customer() {
+        return "customer";
+    }
 
     @RequestMapping("/getSimulateErrorStatus")
     public void getSimulateErrorStatus(HttpServletRequest request, HttpServletResponse response) {
